2f6235886d9459116e0359da25b8ad56efb970bc
doc-sebastian <43200765+doc-sebastian@users.noreply.github.com>
2018-09-16 01:04:15 +0200

    Update sidebar.php

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -6,22 +6,22 @@
             </div>
         </div>
     </div>
-    <h5><strong>DICOM Attributes</strong></h5>
+    <h5><strong><?php print_unescaped($this->l10n->t('DICOM Attributes')); ?></strong></h5>
     <div class="attributes-search-container">
-        <input type="text" class="form-control dicom-attributes-search" placeholder="Search for attributes.." title="Type in an attribute">
+        <input type="text" class="form-control dicom-attributes-search" placeholder="<?php print_unescaped($this->l10n->t('Search for attributes..')); ?>" title="<?php print_unescaped($this->l10n->t('Type in an attribute')); ?>">
     </div>
 
     <div class="sidebar-content-loading">
         <i class="fa fa-spinner fa-spin"></i>
-        <span>Parsing DICOM file...</span>
+        <span><?php print_unescaped($this->l10n->t('Parsing DICOM file...')); ?></span>
     </div>
 
     <div class="sidebar-table-container">
         <table class="dicom-elements-table">
             <thead>
             <tr>
-                <th>Attribute</th>
-                <th>Value</th>
+                <th><?php print_unescaped($this->l10n->t('Attribute')); ?></th>
+                <th><?php print_unescaped($this->l10n->t('Value')); ?></th>
             </tr>
             </thead>
             <tbody></tbody>
