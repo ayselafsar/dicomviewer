732080a0722be842babec4ab7727b8eaf9773673
doc-sebastian <43200765+doc-sebastian@users.noreply.github.com>
2018-09-16 01:15:23 +0200

    Update captureImageDialog.php

--- templates/captureImageDialog.php
+++ templates/captureImageDialog.php
@@ -2,19 +2,19 @@
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
         <div class="modal-header">
-            <h3 class="modal-title">Capture High Quality Image</h3>
+            <h3 class="modal-title"><?php print_unescaped($this->l10n->t('Capture High Quality Image')); ?></h3>
             <button type="button" class="close btn-close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         </div>
         <div class="modal-body">
             <div class="full-width">
-                <h3>Please specify the dimensions, and desired type for the output image.</h3>
+                <h3><?php print_unescaped($this->l10n->t('Please specify the dimensions, and desired type for the output image.')); ?></h3>
             </div>
             <div class="form-content card-round">
                 <div class="form-column">
                     <table class="capture-settings">
                         <tr>
                             <td>
-                                Width (px)
+                                <?php print_unescaped($this->l10n->t('Width (px)')); ?>
                             </td>
                             <td>
                                 <input type="number" id="viewport-preview-width" class="form-control form-control-fixed js-preview-size" data-size="width" value="512" min="1" max="16384">
@@ -26,7 +26,7 @@
                             </td>
                         </tr>
                         <tr>
-                            <td>Height (px)</td>
+                            <td><?php print_unescaped($this->l10n->t('Height (px)')); ?></td>
                             <td>
                                 <input type="number" id="viewport-preview-height" class="form-control form-control-fixed js-preview-size" data-size="height" value="512" min="1" max="16384">
                             </td>
@@ -37,7 +37,7 @@
                 <div class="form-column">
                     <table class="capture-settings">
                         <tr>
-                            <td>File Name</td>
+                            <td><?php print_unescaped($this->l10n->t('File Name')); ?></td>
                             <td>
                                 <input type="text" id="viewport-preview-name" class="form-control" value="image">
                             </td>
@@ -60,12 +60,12 @@
                             <td colspan="2">
                                 <div class="full-width show-annotations-container">
                                     <input type="checkbox" id="showAnnotations" class="form-control-checkbox" name="showAnnotations" checked value="show">
-                                    <label class="form-label-checkbox">Show Annotations</label>
+                                    <label class="form-label-checkbox"><?php print_unescaped($this->l10n->t('Show Annotations')); ?></label>
                                 </div>
                             </td>
                         </tr>
                         <tr>
-                            <td>Image Quality (%)</td>
+                            <td><?php print_unescaped($this->l10n->t('Image Quality (%)')); ?></td>
                             <td>
                                 <input type="number" id="viewport-preview-quality" class="form-control form-control-fixed" data-size="height" value="100" min="1" max="100">
                             </td>
@@ -77,14 +77,14 @@
             <div class="viewport-element-hidden"></div>
             <div class="viewport-preview-wrap">
                 <div class="card-round">
-                    <h4>Image Preview</h4>
+                    <h4><?php print_unescaped($this->l10n->t('Image Preview')); ?></h4>
                     <img class="viewport-preview"/>
                 </div>
             </div>
         </div>
         <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <button type="button" id="downloadImage" class="btn btn-primary btn-image-download">Download</button>
+            <button type="button" class="btn btn-default" data-dismiss="modal"><?php print_unescaped($this->l10n->t('Close')); ?></button>
+            <button type="button" id="downloadImage" class="btn btn-primary btn-image-download"><?php print_unescaped($this->l10n->t('Download')); ?></button>
         </div>
     </div>
   </div>
