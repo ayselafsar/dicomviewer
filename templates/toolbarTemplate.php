e6e2cf0a5584f198c339c63f363616d982f647a8
doc-sebastian <43200765+doc-sebastian@users.noreply.github.com>
2018-09-16 01:01:37 +0200

    Update toolbarTemplate.php

--- templates/toolbarTemplate.php
+++ templates/toolbarTemplate.php
@@ -5,12 +5,12 @@ $seriesPanelOpen = $_['seriesPanelOpen'];
 
 <script id="toolbarTemplate" type="text/x-handlebars-template">
     <div class="toggleSeriesPanel">
-        <div id="toggleSeriesPanelButton" tabindex="1" class="toolbarSectionButton roundedButton toggleSeriesPanelButton <?php if($seriesPanelOpen === 'true') echo 'active'; ?> {{classes}}" title="Toggle Series Panel">
+        <div id="toggleSeriesPanelButton" tabindex="1" class="toolbarSectionButton roundedButton toggleSeriesPanelButton <?php if($seriesPanelOpen === 'true') echo 'active'; ?> {{classes}}" title="<?php print_unescaped($this->l10n->t('Toggle Series Panel')); ?>">
             <div class="svgContainer">
                 <div class="svgContent series-panel"></div>
             </div>
             <div class="buttonLabel">
-                <span>Series</span>
+                <span><?php print_unescaped($this->l10n->t('Series')); ?></span>
             </div>
         </div>
     </div>
@@ -37,13 +37,13 @@ $seriesPanelOpen = $_['seriesPanelOpen'];
                     <i class="fa fa-chevron-down"></i>
                 </div>
                 <div class="buttonLabel">
-                    <span>More</span>
+                    <span><?php print_unescaped($this->l10n->t('More')); ?></span>
                 </div>
             </div>
         </div>
     </div>
 
-    <a class="button-close js-close-viewer" title="Close Viewer"><i class="fa fa-times fa-lg"></i></a>
+    <a class="button-close js-close-viewer" title="<?php print_unescaped($this->l10n->t('Close Viewer')); ?>"><i class="fa fa-times fa-lg"></i></a>
 </script>
 
 <div id="toolbar"></div>
