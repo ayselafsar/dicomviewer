b98e3462e011381196acc73f9eaed66bcfe085d9
Lars1963 <ict@ravenzwaaij.eu>
2018-12-30 21:55:03 +0100

    Update German files

--- js/lib/l10n/de_DE.js
+++ js/lib/l10n/de_DE.js
@@ -33,8 +33,8 @@ OC.L10N.register(
     "Attribute" : "Attribute",
     "Value" : "Wert",
     /* AnnotationDialogs */
-    "Enter your annatotation" : "Notitiz eingeben",
-    "Edit your annatotation" : "Notitiz bearbeiten",
+    "Enter your annatotation" : "Notiz eingeben",
+    "Edit your annatotation" : "Notiz bearbeiten",
     "New label" : "Text",
     "Remove Marker" : "Markierung löschen",
     /* CaptureImageDialog */
