128c3d43bf2b54ee85bf88e2e64fd7d31267cd34
Lars van Ravenzwaaij <ict@ravenzwaaij.eu>
2019-01-13 04:46:39 +0100

    Updated dutch translations (#38)
    * Updated dutch translations
    
    * Re-updated dutch translation
    
    * Small correction

--- l10n/nl.js
+++ l10n/nl.js
@@ -1,45 +1,40 @@
 OC.L10N.register(
     "dicomviewer",
     {
-    /* Toolbar */
-    "Series" : "Series",
-    "Toggle Series Panel" : "Seriepaneel inschakelen",
-    "More" : "Meer",
-    "Stack Scroll" : "Doorloop serie",
-    "Levels" : "Helderheid",
-    "Zoom" : "Zoomen",
+    /* DicomViewer.js */
+    "Open with DICOM Viewer" : "Met DICOM viewer openen",
+    /* ImageLoader.js */
+    "There is no available DICOM image to display" : "Er is geen DICOM-bestand beschikbaar om weer te geven.",
+    /* Toolbar.js */
+    "Stack Scroll" : "Scrollen",
+    "Levels" : "H/K",
+    "ROI Window" : "H/K Venster",
+    "Zoom" : "Zoom",
     "Pan" : "Verplaatsen",
-    "Invert" : "Kleuren omkeren",
-    "Flip H" : "Horizontaal spiegelen",
-    "Flip V" : "Vertikaal spiegelen",
-    "Rotate L" : "Linksom draaien",
-    "Rotate R" : "Rechtsom draaien",
-    "ROI Window" : "ROI-venster",
+    "Invert" : "Kleuromkeer",
+    "Flip H" : "Spiegel H",
+    "Flip V" : "Spiegel V",
+    "Rotate L" : "Rotatie L",
+    "Rotate R" : "Rotatie R",
+    "Reset" : "Reset",
     "Magnify" : "Loep",
-    "Reset" : "Terugzetten",
+    "Length" : "Lengte",
     "Annotate" : "Notitie",
-    "Probe" : "Positie",
-    "Elliptical ROI" : " Ellipsvormige ROI",
-    "Rectangle ROI" : "Rechthoekige ROI",
+    "Probe" : "Beeldpunt",
+    "Elliptical ROI" : "Ovaal",
+    "Rectangle ROI" : "Rechthoek",
     "Angle" : "Hoek",
-    "Clear" : "Terugzetten",
+    "Clear" : "Terug",
     "Capture" : "Export",
-    "Close Viewer" : "Viewer afsluiten",
-    /* Sidebar */
-    "DICOM Attributes" : "DICOM-attributen",
-    "Search for attributes.." : "Zoek naar attributen..",
-    "Type in an attribute" : "Geef een attribuut in",
-    "Parsing DICOM file..." : "DICOM-Bestand analyseren ...",
-    "Attribute" : "Attribuut",
-    "Value" : "Waarde",
-    /* AnnotationDialogs */
+    /* AnnotationDialogs.html */
     "Enter your annotation" : "Notitie ingeven",
     "Edit your annotation" : "Notitie bewerken",
     "New label" : "Tekst",
+    "OK" : "OK",
     "Remove Marker" : "Markering verwijderen",
-    /* CaptureImageDialog */
-    "Capture High Quality Image" : "Afbeelding met hoge kwaliteit exportieren",
-    "Please specify the dimensions, and desired type for the output image." : "Geef a.u.b. de afmetingen und het gewenste type voor de afbeelding aan:",
+    /* CaptureImageDialog.html */
+    "Capture High Quality Image" : "Afbeelding in hoge kwaliteit exportieren",
+    "Please specify the dimensions, and desired type for the output image." : "Geef de afmetingen und het gewenste type voor de afbeelding aan:",
     "Width (px)" : "Breedte (px)",
     "Height (px)" : "Hoogte (px)",
     "File Name" : "Bestandsnaam",
@@ -48,11 +43,29 @@ OC.L10N.register(
     "Image Quality (%)" : "Beeldkwaliteit (%)",
     "Image Preview" : "Voorbeeld",
     "Close" : "Sluiten",
-    "Download" : "Opslaan",
-    /* ViewerMain and ViewerOverlay */
+    "Download" : "Herunterladen",
+    /* Sidebar.html */
+    "DICOM Attributes" : "DICOM-attributen",
+    "Search for attributes.." : "Zoek naar attributen..",
+    "Type in an attribute" : "Geef een attribuut in",
+    "Parsing DICOM file..." : "DICOM-Bestand analyseren ...",
+    "Attribute" : "Attribuut",
+    "Value" : "Waarde",
+    /* Toolbar.html */
+    "Toggle Series Panel" : "Serienvenster inschakelen",
+    "Series" : "Series",
+    "Close Viewer" : "Viewer sluiten",
+    /* ViewerMain.html */
     "Loading..." : "Laden ...",
-    "There is no available DICOM image to display" : "Er is geen DICOM-bestand beschikbaar om weer te geven.",
+    /* ViewportOverlay.html */
     "Ser:" : "Serie:",
-    "Img:" : "Beeld:"
+    "Img:" : "Beeld:",
+    /* renderToolbar.js */
+    "More" : "meer",
+    "Less" : "minder",
+    /* renderViewport.js */
+    "Lossy" : "Verliesend",
+    "Lossless / Uncompressed" : "Verliesloos / Ongecomprimeerd",
+    "WW/WC" : "H/K"
     },
 "nplurals=2; plural=(n != 1);");
