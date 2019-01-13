d59245dc0f559a3f47667bc5c5cefc3a1a8d1d8c
Lars van Ravenzwaaij <ict@ravenzwaaij.eu>
2019-01-13 04:46:04 +0100

    Updated german translations (#39)
    * Updated german translations
    
    * Re-updated german translations

--- l10n/de_DE.js
+++ l10n/de_DE.js
@@ -1,45 +1,40 @@
 OC.L10N.register(
     "dicomviewer",
     {
-    /* Toolbar */
-    "Series" : "Serie(n)",
-    "Toggle Series Panel" : "Serienübersicht einschalten",
-    "More" : "Weitere",
-    "Stack Scroll" : "Serie durchlaufen",
-    "Levels" : "Helligkeit",
-    "Zoom" : "Zoomen",
+    /* DicomViewer.js */
+    "Open with DICOM Viewer" : "Mit DICOM Viewer öffnen",
+    /* ImageLoader.js */
+    "There is no available DICOM image to display" : "Es existiert keine DICOM-Bilddatei zum Anzeigen.",
+    /* Toolbar.js */
+    "Stack Scroll" : "Scrollen",
+    "Levels" : "H/K",
+    "ROI Window" : "H/K Fenster",
+    "Zoom" : "Zoom",
     "Pan" : "Verschieben",
     "Invert" : "Farbumkehr",
-    "Flip H" : "Horizontal spiegeln",
-    "Flip V" : "Vertikal spiegeln",
-    "Rotate L" : "Links drehen",
-    "Rotate R" : "Rechts drehen",
-    "ROI Window" : "ROI-Bereich",
-    "Magnify" : "Lupe",
+    "Flip H" : "Spiegeln H",
+    "Flip V" : "Spiegeln V",
+    "Rotate L" : "Rotieren L",
+    "Rotate R" : "Rotieren R",
     "Reset" : "Reset",
+    "Magnify" : "Lupe",
+    "Length" : "Länge",
     "Annotate" : "Notiz",
-    "Probe" : "Position",
-    "Elliptical ROI" : " Elliptische ROI",
-    "Rectangle ROI" : "Rechtwinklige ROI",
+    "Probe" : "Bildpunkt",
+    "Elliptical ROI" : "Ellipse",
+    "Rectangle ROI" : "Rechteck",
     "Angle" : "Winkel",
     "Clear" : "Löschen",
     "Capture" : "Export",
-    "Close Viewer" : "Viewer schließen",
-    /* Sidebar */
-    "DICOM Attributes" : "DICOM-Attribute",
-    "Search for attributes.." : "Suche nach Attributen..",
-    "Type in an attribute" : "Gesuchtes Attribut eingeben",
-    "Parsing DICOM file..." : "DICOM-Datei analysieren ...",
-    "Attribute" : "Attribute",
-    "Value" : "Wert",
-    /* AnnotationDialogs */
+    /* AnnotationDialogs.html */
     "Enter your annotation" : "Notiz eingeben",
     "Edit your annotation" : "Notiz bearbeiten",
     "New label" : "Text",
+    "OK" : "OK",
     "Remove Marker" : "Markierung löschen",
-    /* CaptureImageDialog */
+    /* CaptureImageDialog.html */
     "Capture High Quality Image" : "Hochauflösendes Bild exportieren",
-    "Please specify the dimensions, and desired type for the output image." : "Bitte geben Sie die Bildgröße und Dateityp der gewünschten Bildausgabe an:",
+    "Please specify the dimensions, and desired type for the output image." : "Bitte geben Sie die Bildgröße und den Dateityp der gewünschten Bildausgabe an:",
     "Width (px)" : "Breite (px)",
     "Height (px)" : "Höhe (px)",
     "File Name" : "Dateiname",
@@ -49,10 +44,28 @@ OC.L10N.register(
     "Image Preview" : "Vorschau",
     "Close" : "Schließen",
     "Download" : "Herunterladen",
-    /* ViewerMain and ViewerOverlay */
+    /* Sidebar.html */
+    "DICOM Attributes" : "DICOM-Attribute",
+    "Search for attributes.." : "Suche nach Attributen..",
+    "Type in an attribute" : "Gesuchtes Attribut eingeben",
+    "Parsing DICOM file..." : "DICOM-Datei analysieren ...",
+    "Attribute" : "Attribute",
+    "Value" : "Wert",
+    /* Toolbar.html */
+    "Toggle Series Panel" : "Serienfenster einschalten",
+    "Series" : "Serien",
+    "Close Viewer" : "Viewer schließen",
+    /* ViewerMain.html */
     "Loading..." : "Lade ...",
-    "There is no available DICOM image to display" : "Es existiert keine DICOM-Bilddatei zum Anzeigen.",
+    /* ViewportOverlay.html */
     "Ser:" : "Serie:",
-    "Img:" : "Bild:"
+    "Img:" : "Bild:",
+    /* renderToolbar.js */
+    "More" : "mehr",
+    "Less" : "weniger",
+    /* renderViewport.js */
+    "Lossy" : "Verlustbehaftet",
+    "Lossless / Uncompressed" : "Verlustfrei / Unkomprimiert",
+    "WW/WC" : "H/K"
     },
 "nplurals=2; plural=(n != 1);");
