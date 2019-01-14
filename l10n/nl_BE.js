8af6789a557456287781cecec17f8e796d9bf84b
Lars1963 <ict@ravenzwaaij.eu>
2019-01-14 20:42:10 +0100

    More translations

---
+++ l10n/nl_BE.js
@@ -0,0 +1,71 @@
+OC.L10N.register(
+    "dicomviewer",
+    {
+    /* DicomViewer.js */
+    "Open with DICOM Viewer" : "Met DICOM viewer openen",
+    /* ImageLoader.js */
+    "There is no available DICOM image to display" : "Er is geen DICOM-bestand beschikbaar om weer te geven.",
+    /* Toolbar.js */
+    "Stack Scroll" : "Scrollen",
+    "Levels" : "H/K",
+    "Levels ROI" : "H/K Venster",
+    "Zoom" : "Zoom",
+    "Pan" : "Verplaatsen",
+    "Invert" : "Kleuromkeer",
+    "Flip H" : "Spiegel H",
+    "Flip V" : "Spiegel V",
+    "Rotate L" : "Rotatie L",
+    "Rotate R" : "Rotatie R",
+    "Reset" : "Reset",
+    "Magnify" : "Loep",
+    "Length" : "Lengte",
+    "Annotate" : "Notitie",
+    "Probe" : "Beeldpunt",
+    "Elliptical ROI" : "Ovaal",
+    "Rectangle ROI" : "Rechthoek",
+    "Angle" : "Hoek",
+    "Clear" : "Terug",
+    "Capture" : "Export",
+    /* AnnotationDialogs.html */
+    "Enter your annotation" : "Notitie ingeven",
+    "Edit your annotation" : "Notitie bewerken",
+    "New label" : "Tekst",
+    "OK" : "OK",
+    "Remove Marker" : "Markering verwijderen",
+    /* CaptureImageDialog.html */
+    "Capture High Quality Image" : "Afbeelding in hoge kwaliteit exportieren",
+    "Please specify the dimensions, and desired type for the output image." : "Geef de afmetingen und het gewenste type voor de afbeelding aan:",
+    "Width (px)" : "Breedte (px)",
+    "Height (px)" : "Hoogte (px)",
+    "File Name" : "Bestandsnaam",
+    "File Type" : "Bestandstype",
+    "Show Annotations" : "Laat notities zien",
+    "Image Quality (%)" : "Beeldkwaliteit (%)",
+    "Image Preview" : "Voorbeeld",
+    "Close" : "Sluiten",
+    "Download" : "Herunterladen",
+    /* Sidebar.html */
+    "DICOM Attributes" : "DICOM-attributen",
+    "Search for attributes.." : "Zoek naar attributen..",
+    "Type in an attribute" : "Geef een attribuut in",
+    "Parsing DICOM file..." : "DICOM-Bestand analyseren ...",
+    "Attribute" : "Attribuut",
+    "Value" : "Waarde",
+    /* Toolbar.html */
+    "Toggle Series Panel" : "Serienvenster inschakelen",
+    "Series" : "Series",
+    "Close Viewer" : "Viewer sluiten",
+    /* ViewerMain.html */
+    "Loading..." : "Laden ...",
+    /* ViewportOverlay.html */
+    "Ser:" : "Serie:",
+    "Img:" : "Beeld:",
+    /* renderToolbar.js */
+    "More" : "meer",
+    "Less" : "minder",
+    /* renderViewport.js */
+    "Lossy" : "Verliesend",
+    "Lossless / Uncompressed" : "Verliesloos / Ongecomprimeerd",
+    "WW/WC" : "H/K"
+    },
+"nplurals=2; plural=(n != 1);");
