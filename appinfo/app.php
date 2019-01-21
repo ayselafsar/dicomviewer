36731a6374e0589b0fd630e2c29cf75e584e7b05
Aysel Afsar <aysel.afsar@gmail.com>
2019-01-20 22:44:55 -0500

    Revert "Only load DICOM viewer CSS/JS on the Files app (#36)"
    This reverts commit 7a90ba76d649004667f816323e18427ea1c0611f.

--- appinfo/app.php
+++ appinfo/app.php
@@ -3,22 +3,16 @@
 namespace OCA\DICOMViewer\AppInfo;
 
 use OCP\Util;
-// Only load CSS/JS on the Files app
-$eventDispatcher = \OC::$server->getEventDispatcher();
-$eventDispatcher->addListener(
-	'OCA\Files::loadAdditionalScripts',
-	function () {
-		Util::addStyle('dicomviewer', 'viewer');
-		Util::addStyle('dicomviewer', 'sidebar');
-		Util::addStyle('dicomviewer', 'bootstrap_accordion');
-		Util::addStyle('dicomviewer', 'viewerMain');
-		Util::addStyle('dicomviewer', 'viewerMainDialog');
-		Util::addStyle('dicomviewer', 'captureImageDialog');
-		Util::addStyle('dicomviewer', 'annotationDialogs');
-		Util::addStyle('dicomviewer', 'external/font-awesome/font-awesome.min');
-		Util::addScript('dicomviewer', 'app.bundle');
-	}
-);
+
+Util::addStyle('dicomviewer', 'viewer');
+Util::addStyle('dicomviewer', 'sidebar');
+Util::addStyle('dicomviewer', 'bootstrap_accordion');
+Util::addStyle('dicomviewer', 'viewerMain');
+Util::addStyle('dicomviewer', 'viewerMainDialog');
+Util::addStyle('dicomviewer', 'captureImageDialog');
+Util::addStyle('dicomviewer', 'annotationDialogs');
+Util::addStyle('dicomviewer', 'external/font-awesome/font-awesome.min');
+Util::addScript('dicomviewer', 'app.bundle');
 
 // Get the server name
 $server_name = '';
