ec95573c77bc5077afc716b84cc9ee5e367ae693
Aysel Afsar <aysel.afsar@gmail.com>
2024-05-25 20:36:34 +0300

    #112 DICOM Viewer Plugin Compatibility with NC 29 (#115)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
+import { generateUrl } from '@nextcloud/router';
 import DICOMView from './views/DICOMView.vue';
 import generateFullUrl from './utils/generateFullUrl.js';
 import registerFileActions from './utils/registerFileActions.js';
 import './sidebar.js';
 
 // Add MimeType Icon
-OC.MimeType._mimeTypeIcons['application/dicom'] = `${generateFullUrl(OC.filePath('dicomviewer', 'img', 'app.svg'))}`;
+OC.MimeType._mimeTypeIcons['application/dicom'] = `${generateFullUrl(generateUrl('/apps/dicomviewer/img/app.svg'))}`;
 
 OCA.Viewer.registerHandler({
     id: 'dicom',
