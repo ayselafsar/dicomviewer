5d0c7a5bbf31512b32ff1699e4270a59b747e24f
Aysel Afsar <aysel.afsar@gmail.com>
2026-06-14 18:18:44 +0300

    #164 Fix reopening dicom sidebar

--- src/sidebar-init.js
+++ src/sidebar-init.js
@@ -11,68 +11,71 @@ Vue.prototype.n = n;
 const TAG_NAME = 'dicomviewer-files-sidebar-tab';
 const SidebarPreviewView = Vue.extend(SidebarPreview);
 
+configureCodecs();
+
 class DicomviewerFilesSidebarTab extends HTMLElement {
-	constructor() {
-		super();
-		this._node = null;
-		this._active = false;
-		this.vueInstance = null;
-	}
 
-	connectedCallback() {
-		if (!this.vueInstance) {
-			configureCodecs();
-			this.vueInstance = new SidebarPreviewView();
-			this.appendChild(this.vueInstance.$mount().$el);
-		}
-		this.syncProps();
-	}
+    constructor() {
+        super();
+        this._node = null;
+        this._active = false;
+        this.vueInstance = null;
+    }
 
-	disconnectedCallback() {
-		if (this.vueInstance) {
-			this.vueInstance.$destroy();
-			this.vueInstance = null;
-		}
-	}
+    connectedCallback() {
+        if (!this.vueInstance) {
+            this.vueInstance = new SidebarPreviewView();
+            this.appendChild(this.vueInstance.$mount().$el);
+        }
+        this.syncProps();
+    }
 
-	get node() {
-		return this._node;
-	}
+    disconnectedCallback() {
+        if (this.vueInstance) {
+            this.vueInstance.$destroy();
+            this.vueInstance = null;
+        }
+    }
 
-	set node(node) {
-		this._node = node;
-		this.syncProps();
-	}
+    get node() {
+        return this._node;
+    }
 
-	get active() {
-		return this._active;
-	}
+    set node(node) {
+        this._node = node;
+        this.syncProps();
+    }
 
-	set active(active) {
-		this._active = !!active;
-		this.syncProps();
-	}
+    get active() {
+        return this._active;
+    }
 
-	syncProps() {
-		if (!this.vueInstance || !this._node) {
-			return;
-		}
-		this.vueInstance.update(this._node, this._active);
-	}
+    set active(active) {
+        this._active = !!active;
+        this.syncProps();
+    }
+
+    syncProps() {
+        if (!this.vueInstance || !this._node) {
+            return;
+        }
+        this.vueInstance.update(this._node, this._active);
+    }
+
 }
 
 getSidebar().registerTab({
-	id: 'dicomviewer',
-	displayName: t('dicomviewer', 'DICOM'),
-	iconSvgInline: AppIcon,
-	order: 50,
-	tagName: TAG_NAME,
-	enabled({ node }) {
-		return node?.type === FileType.File && node.mime === 'application/dicom';
-	},
-	async onInit() {
-		if (!customElements.get(TAG_NAME)) {
-			customElements.define(TAG_NAME, DicomviewerFilesSidebarTab);
-		}
-	},
+    id: 'dicomviewer',
+    displayName: t('dicomviewer', 'DICOM'),
+    iconSvgInline: AppIcon,
+    order: 50,
+    tagName: TAG_NAME,
+    enabled({ node }) {
+        return node?.type === FileType.File && node.mime === 'application/dicom';
+    },
+    async onInit() {
+        if (!customElements.get(TAG_NAME)) {
+            customElements.define(TAG_NAME, DicomviewerFilesSidebarTab);
+        }
+    },
 });
